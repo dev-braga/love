89a19f67a6ffd16d2b53a5d7ddb418b55b0c0f4e
dev-braga <braga.trader12@gmail.com>
2023-06-26 14:24:03 -0300

    Alteração na lógica

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
 
-var btn = document.querySelector('.btn-nao')
+var btn = document.querySelector('.btn-nao');
 
 function desvia(){
 
-   btn.style.position = 'absolute'
-   btn.style.left = gerarPosicao( 10, 80)
-   btn.style.bottom = gerarPosicao( 10, 80)
+   btn.style.position = 'absolute';
+   btn.style.left = gerarPosicao( 10, 80);
+   btn.style.bottom = gerarPosicao( 10, 80);
+
 }
 
 function sim(){
-    window.open('https://wa.me/5571982877132')
+    window.open('https://wa.me/5571982877132');
 }
 
 function gerarPosicao( min, max){
-    return (Math.random() * (max - min) + min) + '%'
+    return (Math.round((Math.random() * (max - min) + min))) + '%';
+    
 }
 
-setInterval( function(){
-    btn.style.position = 'absolute'
-    btn.style.left = gerarPosicao( 10, 90)
-    btn.style.bottom = gerarPosicao( 10, 90)
-}, 400)
+function isMobile(){
+
+    setInterval( function(){
+        btn.style.position = 'absolute';
+        btn.style.left = gerarPosicao( 10, 90);
+        btn.style.bottom = gerarPosicao( 10, 90);
+
+    }, 400)
+}
+
+// Pegar o tamanho da tela e setar para o botão para sempre fugi caso o usuário esteja usando o celular.
+
+var width = window.innerWidth
+
+if(width <= 700){
+   isMobile();
+}
